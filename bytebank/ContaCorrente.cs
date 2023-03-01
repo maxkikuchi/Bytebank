1e203e12ac41d7c440cb6e5674ec6ac85d869013
Max Kikuchi <maxkikuchi@hotmail.com>
2023-03-01 17:56:51 -0300

    commit inicial

---
+++ bytebank/ContaCorrente.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank
+{
+    public class ContaCorrente
+    {
+        public ContaCorrente()
+        {
+
+        }
+
+        private int _myProperty;
+
+        public int MyProperty 
+        { 
+            get 
+            { 
+                return _myProperty; 
+            } 
+            
+            set 
+            { 
+                _myProperty = value; 
+            } 
+        }
+
+
+        public string titular;
+        public string conta;
+        public string agencia;
+        public double saldo;
+
+        public void Depositar(double valor)
+        {
+            this.saldo += valor;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (valor > this.saldo)
+                return false;
+            else
+                this.saldo -= valor;
+
+            return true;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (valor > this.saldo)
+            {
+                return false;
+            }
+            else
+            {
+                this.saldo -= valor;
+                contaDestino.saldo += valor;
+                return true;
+            }
+        }
+    }
+}
---
+++ bytebank/Program.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using bytebank;
+
+bool finalizar = true;
+
+ContaCorrente contaMax = new ContaCorrente()
+{
+    agencia = "4092",
+    conta = "001",
+    saldo = 1000
+};
+
+ContaCorrente contaDestino = new ContaCorrente()
+{
+    agencia = "4092",
+    conta = "002",
+    saldo = 0
+};
+
+while (finalizar)
+{
+    Console.WriteLine("1-Sacar");
+    Console.WriteLine("2-Depositar");
+    Console.WriteLine("3-Transferir");
+
+    switch (Console.ReadLine())
+    {
+        case "1":
+            Console.WriteLine("Informe o valor (saque):");
+            double.TryParse(Console.ReadLine(), out double valorSaque);
+            if (!contaMax.Sacar(valorSaque))
+                Console.WriteLine("Saldo insuficiente!");
+            break;
+        case "2":
+            Console.WriteLine("Informe o valor (depósito):");
+            double.TryParse(Console.ReadLine(), out double valorDeposito);
+            contaMax.Depositar(valorDeposito);
+            break;
+        case "3":
+            Console.WriteLine("Informe o valor (transferência):");
+            double.TryParse(Console.ReadLine(), out double valorTransferir);
+            if (!contaMax.Transferir(valorTransferir, contaDestino))
+                Console.WriteLine("Saldo insuficiente!");
+            break;
+        default:
+            break;
+    }
+
+    Console.WriteLine(string.Format("Valor saldo Max: {0}", contaMax.saldo.ToString("0.00")));
+    Console.WriteLine(string.Format("Valor saldo Destino: {0}", contaDestino.saldo.ToString("0.00")));
+
+    Console.WriteLine("Deseja continuar? (S/N)");
+    if (Console.ReadLine() == "N")
+    {
+        break;
+    }
+};
+
+
+
