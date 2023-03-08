d519ff0f1e30f59a912b37b92b777883e708b6d0
Max Kikuchi <maxkikuchi@hotmail.com>
2023-03-08 18:37:13 -0300

    Modularização de Requisições

---
+++ Bytebank/Infrastructure/ManipuladorRequisicaoArquivo.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bytebank.Infrastructure
+{
+    public class ManipuladorRequisicaoArquivo
+    {
+        public void Manipular(HttpListenerResponse resposta, string path)
+        {
+            string contentType = string.Empty;
+            string nomeResource = string.Empty;
+
+            switch (path)
+            {
+                case "/favicon.ico":
+                    string fullPath = $"/Assets/images{path}";
+
+                    nomeResource = Utility.ConverterPathParaNomeAssembly(fullPath);
+                    contentType = Utility.ObterTipoDeConteudo(fullPath);
+                    
+                    break;
+                default:
+                    nomeResource = Utility.ConverterPathParaNomeAssembly(path);
+                    contentType = Utility.ObterTipoDeConteudo(path);
+                    
+                    break;
+            }
+
+            var assembly = Assembly.GetExecutingAssembly();
+
+            var resourceStream = assembly.GetManifestResourceStream(nomeResource);
+
+            if (resourceStream == null)
+            {
+                resposta.StatusCode = 404;
+            }
+            else
+            {
+                int streamLength = (int)resourceStream.Length;
+                var bytesResource = new byte[streamLength];
+
+                using (resourceStream)
+                {
+                    resourceStream.Read(bytesResource, 0, streamLength);
+                }
+
+                resposta.ContentType = contentType;
+                resposta.StatusCode = 200;
+                resposta.ContentLength64 = streamLength;
+                resposta.OutputStream.Write(bytesResource, 0, streamLength);
+            }
+
+            resposta.OutputStream.Close();
+        }
+    }
+}
